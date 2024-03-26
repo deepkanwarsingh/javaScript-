8879f6093cdc1529dff3831922993d425de84db2
Deepkanwaar Singh <deepkanwarmann@gmail.com>
2024-03-26 23:40:24 +0530

    changes

--- 2/index.js
+++
@@ -1,9 +0,0 @@
-
-// console.log("hello")
-window.addEventListener("mousemove", function(details){
-    gsap.to("#center" ,{
-        left : details.clientX+"px",
-        // ease: power2,
-       
-    })
-})
